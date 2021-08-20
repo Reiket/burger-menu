ffdce4f058741e34f3f9ae7828e53e8244f59ff3
Reiket <73478093+Reiket@users.noreply.github.com>
2021-08-20 19:17:57 +0300

    r

---
+++ js/index.js
@@ -0,0 +1,71 @@
+//щоб опреділити на якому устройстві відкрито сайт (чи тачскрін чи мишка)
+
+const isMobile = {
+   Android: function () {
+      return navigator.userAgent.match(/Android/i);
+   },
+   BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+   },
+   IOS: function () {
+      return navigator.userAgent.match(/IPhone|IPad|IPod/i);
+   },
+   Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+   },
+   Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+   },
+   any: function () {
+      return (
+         isMobile.Android() ||
+         isMobile.BlackBerry() ||
+         isMobile.IOS() ||
+         isMobile.Opera() ||
+         isMobile.Windows());
+   }
+};
+
+if (isMobile.any()) { //перевіряєм яке устройство
+   document.body.classList.add('_touch'); //якщо с тачскріном, то добавляєм в body клас _touch
+
+   let menuArrows = document.querySelectorAll('.menu__arrow');//добавляєм в переміну menuArrows всі блоки з класом menu__arrow
+   if (menuArrows.length > 0) { // перевіряєм чи вони існують
+      for (let index = 0; index < menuArrows.length; index++) { //перебираєм їх
+         const menuArrow = menuArrows[index];
+         menuArrow.addEventListener('click', (e) => { //добавляєм подію клік
+            menuArrow.parentElement.classList.toggle('_active'); //добавляєм родителю клас active
+         });
+      }
+   }
+} else {
+   document.body.classList.add('_pc'); //якщо це пк добавляэм в body клас _pc
+}
+
+
+//прокрутка при клике
+
+const menuLinks = document.querySelectorAll('menu__link', 'data-goto'); //добавляєм в переміну menuLinks всі блоки з класом menu__link[data-goto]
+
+if (menuLinks.length > 0) { // перевіряєм чи вони існують
+   menuLinks.forEach(menuLink => { //перебираєм їх
+      menuLink.addEventListener('click', onMenuLinkClick); //добавляєм подію клік
+   });
+
+   function onMenuLinkClick(e) {
+      const menuLink = e.target; //получаєм об'єкт на який ми клікнули
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) { //перевірєм чи цей атрибут заповнений і чи існує об'єкт на який ми клікаємо
+         const gotoBlock = document.querySelector(menuLink.dataset.goto); //получаєм цей об'єкт в перемінну
+         //виміряєм на скілбки пікселів потрібно прокрутити
+         const gotoBlockValue = gotoBlock.getBoudingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+         window.scrollTo({ //добавляэм скролл
+            top: gotoBlockValue,
+            behavior: "smooth" //прокрутка плавна
+         });
+         e.preventDefault();
+      }
+   }
+}
+
+console.log(menuLinks);
